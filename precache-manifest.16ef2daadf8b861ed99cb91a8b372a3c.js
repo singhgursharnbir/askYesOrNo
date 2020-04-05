fbb97209abff6602d9a16a38ebdb2ec62f644bf7
Gursharnbir Singh <singhgursharnbir7511@gmail.com>
2020-04-05 15:05:39 -0600

    Updates

---
+++ precache-manifest.16ef2daadf8b861ed99cb91a8b372a3c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "205ea728d956466a0b25e4edff881ad2",
+    "url": "/askYesOrNo/index.html"
+  },
+  {
+    "revision": "3fec1986685ad80baee4",
+    "url": "/askYesOrNo/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "7d819b4cddd15b8e4451",
+    "url": "/askYesOrNo/static/css/main.2ddcca98.chunk.css"
+  },
+  {
+    "revision": "3fec1986685ad80baee4",
+    "url": "/askYesOrNo/static/js/2.a4f340e7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/askYesOrNo/static/js/2.a4f340e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d819b4cddd15b8e4451",
+    "url": "/askYesOrNo/static/js/main.8f7e892c.chunk.js"
+  },
+  {
+    "revision": "361a2cac079f586129f0",
+    "url": "/askYesOrNo/static/js/runtime-main.e70dd62d.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/reactAskUniverse/precache-manifest.de7cba1b4c1470e8b5e517b6f7c91146.js"
+  "/askYesOrNo/precache-manifest.16ef2daadf8b861ed99cb91a8b372a3c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/reactAskUniverse/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/askYesOrNo/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
